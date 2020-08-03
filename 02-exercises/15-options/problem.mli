3d2dc9f384ff3016b937d188fd982487bc19fc01
Lucas Weng <hsiangenw@gmail.com>
2020-08-04 07:55:27 +0800

    Finish 15-options

--- 02-exercises/15-options/problem.mli
+++ 02-exercises/15-options/problem.mli
@@ -6,4 +6,5 @@ type 'a option =
 
 val safe_divide : dividend:int -> divisor:int -> int option
 val option_concatenate : string option -> string option -> string option
-val concatenate : ?separator : string -> string -> string -> string
+val concatenate : ?separator:string -> string -> string -> string
+val better_labeled_concatenate : ?separator:string -> string1:string -> string2:string -> unit -> string
